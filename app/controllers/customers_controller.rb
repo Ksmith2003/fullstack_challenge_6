63b1b0a51f66b6cc5ae3d124d6664bbfb1dae5b1
Kayla Smith <kaymsmith2003@gmail.com>
2025-11-03 23:50:51 -0600

    got parts 1 and 2 done (i did both at the same time, so thats why its one commit)

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,13 @@
+class CustomersController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+
+  def missing_email
+    @customers = Customer.where(email_address: nil)
+  end
+
+  def alphabetized
+    @customers = Customer.order("full_name ASC")
+  end
+end
